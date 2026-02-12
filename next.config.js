8a4c4b314883f44410b5d72b1dad43101915be05
Sam <sam@Sams-Mac-mini.local>
2026-02-11 23:26:00 -0500

    Update next.config.js to alias @prisma/client/index-browser to false

--- next.config.js
+++ next.config.js
@@ -2,17 +2,20 @@
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // Don't try to bundle Prisma for the browser
+      // Don't bundle Prisma browser client - it doesn't exist on Vercel
       config.resolve.fallback = {
         ...config.resolve.fallback,
         fs: false,
         net: false,
         tls: false,
       };
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@prisma/client/index-browser': false,
+      };
     }
     return config;
   },
-  transpilePackages: ['@prisma/client'],
 };
 
 module.exports = nextConfig;
