bc8c958d16e32f434b1ff7b2928500c020ce281a
Sam <sam@Sams-Mac-mini.local>
2026-02-12 18:05:30 -0500

    Fix NextAuth handler export

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -91,6 +91,6 @@ export const authOptions = {
   secret: process.env.AUTH_SECRET,
 };
 
-const { handler, signIn, signOut, auth } = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, signIn, signOut, auth };
+export { handler as GET, handler as POST };
